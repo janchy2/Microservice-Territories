4414bdeccece778e56ab6ba61ff035b458c94a66
Jana <sj210381d@student.etf.bg.ac.rs>
2024-10-21 11:25:50 +0200

    finished docker-compose

--- application/common_functions.py
+++ application/common_functions.py
@@ -4,10 +4,27 @@ from fastapi.responses import JSONResponse
 
 
 def connect_to_database():
-    dynamodb_endpoint = os.getenv("DYNAMODB_ENDPOINT", "http://localhost:8001")
-    dynamodb = boto3.resource("dynamodb", endpoint_url=dynamodb_endpoint)
+    dynamodb_endpoint = os.getenv("DYNAMODB_ENDPOINT")
+    dynamodb = boto3.resource(
+        "dynamodb", region_name="eu-south-1", endpoint_url=dynamodb_endpoint
+    )
+    return create_table_if_not_exists(dynamodb)
+
+
+def create_table_if_not_exists(dynamodb):
     table_name = "territories"
-    return dynamodb.Table(table_name)
+    try:
+        table = dynamodb.Table(table_name)
+        table.load()
+    except dynamodb.meta.client.exceptions.ResourceNotFoundException:
+        table = dynamodb.create_table(
+            TableName=table_name,
+            KeySchema=[{"AttributeName": "uuid", "KeyType": "HASH"}],
+            AttributeDefinitions=[{"AttributeName": "uuid", "AttributeType": "S"}],
+            ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
+        )
+        table.wait_until_exists()
+    return table
 
 
 def generate_response(status, message):
